92f7fc484e2480995a852c5f8be7269bb5a02972
Venkata Ramana <venkata.ramana@ad3ihyd.local>
2020-06-26 17:28:44 +0530

    home page changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import  HomePage  from './homepage.components';
+import  HomePage  from './pages/homepage/homepage.component';
 
 function App() {
   return <div><HomePage /></div>
